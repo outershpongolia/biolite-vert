6d28d27d726aad4a7edc8367a1566a1d7583c447
Milena <milenavuckovic3@gmail.com>
2022-06-29 18:26:09 +0200

    added menu for mobile

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import _ from 'lodash';
 import './style.scss';
+
+const menuIcon = document.getElementById("js-open");
+const closeIcon = document.getElementById("js-close");
+const menu = document.getElementById("js-menu")
+
+menuIcon.addEventListener('click', function(e) {
+    menu.classList.add('active');
+})
+
+closeIcon.addEventListener('click', function(e) {
+    menu.classList.remove('active');
+})
